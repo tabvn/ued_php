8e3dfd29e602bb2c99ea1c6d2ca72bf8053cd727
Toan <toan@tabvn.com>
2020-05-30 21:15:18 +0700

    add database schema

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<html>
+<?php
+$mysqli = new mysqli("127.0.0.1","root","root","ued");
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+$sql = "SELECT first_name, last_name FROM teachers";
+$result  = $mysqli -> query($sql);
+// Numeric array
+$row = $result -> fetch_array(MYSQLI_NUM);
+printf ("%s (%s)\n", $row[0], $row[1]);
+$teachers = array();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+</head>
 <body>
-<h1>
-    <?php print("Hello world"); ?>
-</h1>
 </body>
 </html>
