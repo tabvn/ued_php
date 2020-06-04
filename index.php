d4e611bbf8a28de6330e55c48da2bc83b9872b48
Toan <toan@tabvn.com>
2020-06-04 09:14:42 +0700

    add bootstrap

--- index.php
+++ index.php
@@ -2,18 +2,95 @@
 require('./mysql.php');
 $conn = connect();
 $sql = "SELECT email, password FROM users";
-$result  = $conn -> query($sql);
+
+/*$result  = $conn -> query($sql);
 // Numeric array
 $row = $result -> fetch_array(MYSQLI_NUM);
 printf ("%s (%s)\n", $row[0], $row[1]);
 $teachers = array();
+*/
+
+//$conn->query("INSERT INTO giang_vien (first_name, last_name) VALUES ('B','Nguyen Van')");
+if (isset($_POST['first_name']) && isset($_POST['last_name'])) {
+    $firstName = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    if (!empty($firstName) && !empty($last_name)) {
+        $conn->query("INSERT INTO giang_vien (first_name, last_name) VALUES ('" . $firstName . "', '" . $last_name . "')");
+    } else {
+        print("Co loi xay ra!.");
+    }
+
+}
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 </head>
 <body>
+<form action="index.php" method="POST">
+    <div>
+        <label>
+            First
+        </label>
+        <input name="first_name" type="text"/>
+    </div>
+
+    <div>
+        <label>
+            Last
+        </label>
+        <input name="last_name" type="text"/>
+    </div>
+
+    <button type="submit">Them Giang Vien</button>
+</form>
+
+</form>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+    </tr>
+    </tbody>
+</table>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
 </body>
 </html>
