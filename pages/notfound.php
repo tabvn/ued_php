b4d2eddd6bac2083b375aa113fab00df2ebfe77b
Toan <toan@tabvn.com>
2020-06-09 22:02:27 +0700

    Add page not found

--- pages/notfound.php
+++ pages/notfound.php
@@ -1,3 +1,13 @@
-<section>
-    <h1>Page not found!</h1>
+<section class="hero is-medium is-primary">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-8-desktop is-offset-2-desktop">
+                    <h1 class="title is-2 is-spaced">
+                        Error 404!
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
