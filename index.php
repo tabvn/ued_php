0a57ce3063134214b65a2ff66c1c6d7efb823a87
Toan <toan@tabvn.com>
2020-06-11 17:28:00 +0700

    create user

--- index.php
+++ index.php
@@ -9,8 +9,44 @@ require_once "./functions.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UED</title>
     <link rel="stylesheet" href="<?php print path("assets/css/main.css"); ?>">
+    <link rel="stylesheet" href="<?php print path("assets/css/style.css"); ?>">
 </head>
 <body>
+<nav class="bd-navbar navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="<?php print path("/"); ?>">
+                <h1 class="title">UED</h1>
+            </a>
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+               data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Trang chủ
+                </a>
+                <a class="navbar-item">
+                    Đăng ký học phần
+                </a>
+            </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-light">
+                           Đăng Nhập
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
 <?php
 renderPage();
 ?>
---
+++ pages/admin/users/create.php
@@ -0,0 +1,67 @@
+<?php
+$values = array('email' => "", 'password' => "");
+$errors = null;
+if (isset($_POST)) {
+    $values['email'] = trim($_POST['email']);
+    $values['password'] = $_POST['password'];
+    if (empty($values['email'])) {
+        $errors['email'] = "Địa chỉ email là bắt buộc";
+    }
+    if (empty($values['password'])) {
+        $errors['password'] = "Mật khẩu là bắt buộc";
+    }
+    if ($errors == null) {
+        // handle create user
+        $db = Database::getConnection();
+        var_dump($db);
+    }
+}
+?>
+<div id="content">
+    <div class="container">
+        <div class="columns is-mobile is-centered">
+            <div class="column is-half">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-header-title">Tạo tài khoản sinh viên</div>
+                    </div>
+                    <div class="card-content">
+                        <form method="post" action="<?php print path('/index.php?p=admin/users/create'); ?>">
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input name="email"
+                                           class="input <?php print $errors['email'] ? "is-danger" : ""; ?>"
+                                           type="email" placeholder="Địa chỉ email"
+                                           value="<?php print $values['email'] ?>">
+                                    <?php if (!empty($errors['email'])): ?>
+                                        <p class="help is-danger"><?php print $errors['email']; ?></p>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Mật khẩu</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input name="password"
+                                           class="input <?php print $errors['password'] ? 'is-danger' : ''; ?>"
+                                           type="password" placeholder="Mật khẩu"
+                                           value="<?php print $values['password'] ?>"
+                                    >
+                                    <?php if (!empty($errors['password'])): ?>
+                                        <p class="help is-danger"><?php print $errors['password']; ?></p>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <button type="submit" class="button is-link">Tạo tài khoản</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
