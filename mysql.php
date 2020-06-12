1d85c0ff334898e6b5e28ff0d95c79cbc1b14872
= <=>
2020-06-12 08:11:53 +0700

    commit

--- mysql.php
+++ mysql.php
@@ -1,6 +1,7 @@
 <?php
 function connect(){
-    $mysqli = new mysqli("127.0.0.1","root","root","ued");
+    $mysqli = new mysqli("localhost","root","root","ued");
+
 // Check connection
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
