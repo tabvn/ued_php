6c8117f57ff13f6b500717e5407d3eb22c44db13
Toan <toan@tabvn.com>
2020-06-13 16:32:17 +0700

    add admin menu

---
+++ admin_menu.php
@@ -0,0 +1,25 @@
+<div class="column">
+    <aside class="menu">
+        <p class="menu-label">
+            Sinh viên
+        </p>
+        <ul class="menu-list">
+            <li><a href="<?php print path('/?p=admin/students'); ?>">Tất cả sinh viên</a></li>
+            <li><a href="<?php print path("/?p=admin/students/create"); ?>">Thêm sinh viên</a></li>
+        </ul>
+        <p class="menu-label">
+            Học phần
+        </p>
+        <ul class="menu-list">
+            <li><a>Học phần đang mở</a></li>
+            <li><a>Mở học phần mới</a></li>
+        </ul>
+        <p class="menu-label">
+            Quản trị viên
+        </p>
+        <ul class="menu-list">
+            <li><a>Tất cả quản trị viên</a></li>
+            <li><a>Thêm quản trị viên</a></li>
+        </ul>
+    </aside>
+</div>
