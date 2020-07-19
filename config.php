b24a83e3bd94e973ce7cdda13ce9c98daee4fcf7
= <=>
2020-07-19 10:31:02 +0700

    ...

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ define("DB", "ued");
 define("HOST", "127.0.0.1");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
-define("PATH", "/~toan/ued/"); // /~toan/ued  nhớ đổi thông số này nhé nếu các bạn thì có thể đổi sang "/ued/"
+define("PATH", "/ued/"); // /~toan/ued  nhớ đổi thông số này nhé nếu các bạn thì có thể đổi sang "/ued/"
