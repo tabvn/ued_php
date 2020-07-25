8c83c3b975deebeb4b7f5bf01456d283a730cb17
Toan <toan@tabvn.com>
2020-07-25 17:22:04 +0700

    home

--- pages/home.php
+++ pages/home.php
@@ -3,11 +3,65 @@
     <div class="hero-body">
         <div class="container">
             <div class="columns">
-                <div class="column is-8-desktop is-offset-2-desktop">
+                <div class="column">
                     <h1 class="title is-2 is-spaced">
-                        Hello UED!
+                        <?php
+                        date_default_timezone_set('Asia/Ho_Chi_Minh');
+                        $Hour = date('G');
+                        if ( $Hour >= 5 && $Hour <= 11 ) {
+                            echo "Good Morning";
+                        } else if ( $Hour >= 12 && $Hour <= 18 ) {
+                            echo "Good Afternoon";
+                        } else if ( $Hour >= 19 || $Hour <= 4 ) {
+                            echo "Good Evening";
+                        }
+                        ?> UEDer!
                     </h1>
+                    <h2 class="subtitle">
+                        Web đăng ký học phần trực tuyến.
+                    </h2>
                 </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section">
+    <div class="container">
+        <h2 class="title">The Team</h2>
+        <div class="columns">
+            <div class="column">
+                <article class="team box">
+                    <div class="box-content">
+                        <div class="avatar">
+                            <img src="<?php print path('/assets/images/duy_binh_doan.jpg')?>"  alt=""/>
+                        </div>
+                        <h2 class="title has-text-centered">Đoàn Duy Bình</h2>
+                        <h3 class="subtitle has-text-centered">Lecturer</h3>
+                    </div>
+                </article>
+            </div>
+            <div class="column">
+                <article class="team box">
+                    <div class="box-content">
+                        <div class="avatar">
+                            <img src="<?php print path('/assets/images/duy_binh_doan.jpg')?>"  alt=""/>
+                        </div>
+                        <h2 class="title has-text-centered">Đoàn Duy Bình</h2>
+                        <h3 class="subtitle has-text-centered">Lecturer</h3>
+                    </div>
+                </article>
+            </div>
+            <div class="column">
+                <article class="team box">
+                    <div class="box-content">
+                        <div class="avatar">
+                            <img src="<?php print path('/assets/images/duy_binh_doan.jpg')?>"  alt=""/>
+                        </div>
+                        <h2 class="title has-text-centered">Đoàn Duy Bình</h2>
+                        <h3 class="subtitle has-text-centered">Lecturer</h3>
+                    </div>
+                </article>
             </div>
         </div>
     </div>
