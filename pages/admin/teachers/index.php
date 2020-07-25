335df79a9f56b92811a1cd1c76d27d384ce50d82
= <=>
2020-07-25 11:26:04 +0700

    edit teacher

--- pages/admin/teachers/index.php
+++ pages/admin/teachers/index.php
@@ -1,30 +1,36 @@
 <?php
-$teachers = array();
+
+$students = array();
 $message = null;
 $db = Database::getConnection();
 // delete
-if(!empty($_POST) && !empty($_POST['delete_id'])){
-    $delete_id = $_POST['delete_id'];
-    $s = $db->prepare("DELETE FROM giang_vien WHERE id = ?");
-    $s->bind_param("i",$delete_id);
-    if ( ! $s->execute()) {
-        $message = array('type' => 'success', 'message' => 'Lỗi xảy ra: '.$s->error);
-
+if ( ! empty($_POST) && ! empty($_POST['delete_id'])) {
+    $id = (int) $_POST['delete_id'];
+    $q = $db->prepare("DELETE FROM giang_vien WHERE id = ?");
+    $q->bind_param("i", $id);
+    if ( ! $q->execute()) {
+        $message = array(
+            'type'    => 'error',
+            'message' => "Có lỗi xảy ra: ".htmlspecialchars($q->error),
+        );
     } else {
-        $message = array('type'    => 'success',
-                         'message' => 'Xoá thành công',
+        $message = array(
+            'type' => 'success', 'message' => "Xoá thành công",
         );
     }
-    $s->close();
 }
 // get
-$stmt = $db->prepare("SELECT id, ho, ten, DATE_FORMAT(ngay_sinh, '%d/%m/%Y') AS ngay_sinh ,email, dien_thoai FROM giang_vien ORDER BY id");
-if (!$stmt->execute()) {
-    $message = array('type' => 'error', 'message' => htmlspecialchars($stmt->error));
+$stmt
+    = $db->prepare("SELECT id, ho, ten, DATE_FORMAT(ngay_sinh, '%d/%m/%Y') as ngay_sinh, email, dien_thoai FROM giang_vien order by id asc LIMIT 0,10");
+//$stmt->bind_param("i", 10);
+if ( ! $stmt->execute()) {
+    $message = array(
+        'type' => 'error', 'message' => htmlspecialchars($stmt->error),
+    );
 } else {
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
-        $teachers[] = $row;
+        $students[] = $row;
     }
 }
 $stmt->close();
@@ -33,51 +39,75 @@ require_once "header.php";
 <div id="content">
     <div class="container">
         <div class="columns">
-            <?php require_once "admin_menu.php"; ?>
+            <?php
+            require_once "admin_menu.php"; ?>
             <div class="column is-10">
                 <div class="card">
                     <div class="card-header">
                         <div class="card-header-title">Danh sách giảng viên</div>
                     </div>
                     <div class="card-content">
-                        <?php if (!empty($message)): ?>
+                        <?php
+                        if ( ! empty($message)): ?>
                             <article
-                                    class="message <?php print $message['type'] == 'error' ? 'is-danger' : 'is-success' ?>">
+                                    class="message <?php
+                                    print $message['type'] == 'error'
+                                        ? 'is-danger' : 'is-success' ?>">
                                 <div class="message-body">
-                                    <?php print $message['message']; ?>
+                                    <?php
+                                    print $message['message']; ?>
                                 </div>
                             </article>
-                        <?php endif; ?>
+                        <?php
+                        endif; ?>
                         <table class="table">
                             <thead>
                             <tr>
-                                <th>ID</th>
                                 <th>Họ Tên</th>
                                 <th>Ngày sinh</th>
                                 <th>Email</th>
-                                <th>Điện Thoại</th>
+                                <th>SĐT</th>
                                 <th></th>
                             </tr>
                             </thead>
                             <tbody>
-                            <?php foreach ($teachers as $teacher): ?>
+                            <?php
+                            foreach ($students as $student): ?>
                                 <tr>
-                                    <td><?php print $teacher['id'] ?></td>
-                                    <td><?php print $teacher['ho'] . " " . $teacher['ten'] ?></td>
-                                    <td><?php print $teacher['ngay_sinh'] ?></td>
-                                    <td><?php print $teacher['email'] ?></td>
-                                    <td><?php print $teacher['dien_thoai'] ?></td>
+                                    <td><?php
+                                        print $student['ho']." "
+                                            .$student['ten'] ?></td>
+                                    <td><?php
+                                        print $student['ngay_sinh'] ?></td>
+                                    <td><?php
+                                        print $student['email'] ?></td>
+                                    <td><?php
+                                        print $student['dien_thoai'] ?></td>
                                     <td>
                                         <div class="is-flex">
-                                            <button type="button" class="button is-text">Sửa</button>
-                                            <form id="form-<?php print $teacher['id']?>" action="<?php print currentUrl();?>" method="post">
-                                                <input name="delete_id" type="hidden" value="<?php print $teacher['id'];?>">
-                                                <button type="submit" class="button is-text">Xoá</button>
+                                            <button class="button is-text"><a
+                                                        href="<?php
+                                                        print path("?p=admin/teachers/edit&id=")
+                                                            .$student['id']; ?>">Sửa</a>
+                                            </button>
+                                            <form method="post" action="<?php
+                                            print currentUrl(); ?>"
+                                                  id="form-<?php
+                                                  print $student['id'] ?>">
+                                                <input name="delete_id"
+                                                       value="<?php
+                                                       print $student['id'] ?>"
+                                                       type="hidden">
+                                                <button type="submit"
+                                                        class="button is-text">
+                                                    Xoá
+                                                </button>
                                             </form>
                                         </div>
                                     </td>
                                 </tr>
-                            <?php endforeach; ?>
+                            <?php
+                            endforeach; ?>
                             </tbody>
                         </table>
                     </div>
