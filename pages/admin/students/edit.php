c8c0296bf48b997722253f2a52b7de1a6c5a6d89
Toan <toan@tabvn.com>
2020-07-25 13:17:17 +0700

    edit student

--- pages/admin/students/edit.php
+++ pages/admin/students/edit.php
@@ -1,56 +1,23 @@
 <?php
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 if (empty($_GET['id'])) {
     redirect('?p=notfound');
 }
 $values = array(
-<<<<<<< HEAD
-  'id'          => "",
-  'email' => "",
-  'password' => "",
-  'ma_sinh_vien' => "",
-  'ten' =>  "",
-  'ho' => "",
-
-  'lop' => "",
-
-=======
   'ma_sinh_vien' => '',
   'ten'          => "",
   'ho'           => '',
   'ngay_sinh'    => "",
   'lop'          => "",
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 );
 $errors = null;
 $message = null;
 
-<<<<<<< HEAD
-function getSubject($id)
-=======
 function getStudent($id)
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 {
     $id = (int) $id;
     $db = Database::getConnection();
 
-<<<<<<< HEAD
-    return $db->query("SELECT id, email FROM users WHERE id = $id")
-      ->fetch_assoc();
-}
-
-function editSubject($values)
-{
-    $db = Database::getConnection();
-    $hash =md5($values['password']);
-    $stmt = $db->prepare("UPDATE ued.sinh_vien SET ma_sinh_vien =? ,ten =? ,ho=?,lop=? WHERE user_id = ?");
-    $stmt->bind_param("ssssi", $values['ma_sinh_vien'], $values['ten'],$values['ho'],$values['lop'],$id);
-=======
     return $db->query("SELECT id,ma_sinh_vien, ten, ho, DATE_FORMAT(ngay_sinh, '%d/%m/%Y') as ngay_sinh,lop FROM sinh_vien WHERE id = $id")
       ->fetch_assoc();
 }
@@ -62,7 +29,6 @@ function editStudent($values)
       = $db->prepare("UPDATE sinh_vien SET ma_sinh_vien=?,ten=?,ho=?,ngay_sinh=?,lop=? WHERE id = ?");
     $stmt->bind_param("sssssi", $values['ma_sinh_vien'], $values['ten'],
       $values['ho'], $values['ngay_sinh'], $values['lop'], $values['id']);
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
     if ( ! $stmt->execute()) {
         return $stmt->error;
     }
@@ -72,19 +38,6 @@ function editStudent($values)
 }
 
 if ( ! empty($_POST)) {
-<<<<<<< HEAD
-  //  $values['id'] = $_POST['id'];
-    $values['email'] = $_POST['email'];
-    $values['password'] =$_POST['password'];
-    if (empty($values['password'])) {
-        $errors['password'] = 'Bạn phải nhập password';
-    }
-    if (empty($values['email'])) {
-        $errors['email'] = 'Bạn phải nhập vào Email';
-    }
-    if ($errors == null) {
-        $error = editSubject($values);
-=======
     $values['lop'] = $_POST['lop'];
     $values['ma_sinh_vien'] = $_POST['ma_sinh_vien'];
     $values['ngay_sinh'] = $_POST['ngay_sinh'];
@@ -119,7 +72,6 @@ if ( ! empty($_POST)) {
     }
     if ($errors == null) {
         $error = editStudent($values);
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
         if ( ! empty($error)) {
             $message = array(
               'type'    => 'error',
@@ -134,23 +86,6 @@ if ( ! empty($_POST)) {
     }
 }
 $id = $_GET['id'];
-<<<<<<< HEAD
-$user = getSubject($id);
-if (empty($user)) {
-    redirect('?p=notfound');
-}
-$values['id'] = $user['id'];
-$values['email'] = $user['email'];
-
-require_once "header.php";
-
-?>
-
-<div id="content">
-    <div class="container">
-        <div class="columns">
-            <?php require_once "admin_menu.php"; ?>
-=======
 $obj = getStudent($id);
 if (empty($obj)) {
     redirect('?p=notfound');
@@ -169,53 +104,11 @@ require_once "header.php";
         <div class="columns">
             <?php
             require_once "admin_menu.php"; ?>
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
             <div class="column is-9">
                 <div class="columns">
                     <div class="column is-9">
                         <div class="card">
                             <div class="card-header">
-<<<<<<< HEAD
-                                <div class="card-header-title">Cập nhật sinh viên</div>
-                            </div>
-                            <div class="card-content">
-                                <?php if (!empty($message)): ?>
-                                    <article
-                                        class="message <?php print $message['type'] == 'error' ? 'is-danger' : 'is-success' ?>">
-                                        <div class="message-body">
-                                            <?php print $message['message']; ?>
-                                        </div>
-                                    </article>
-                                <?php endif; ?>
-                                <form method="post" action="<?php print path('/index.php?p=admin/students/edit&id=').$id; ?>">
-                                    <div class="field">
-                                        <label class="label">Email</label>
-                                        <div class="control">
-                                            <input name="email"
-                                                   class="input <?php print !empty($errors['email']) ? "is-danger" : ""; ?>"
-                                                   type="email" placeholder="Địa chỉ email"
-                                                   value="<?php print $values['email'] ?>"
-                                            >
-                                            <?php if (!empty($errors['email'])): ?>
-                                                <p class="help is-danger"><?php print $errors['email']; ?></p>
-                                            <?php endif; ?>
-                                        </div>
-
-                                    </div>
-                                    <div class="field">
-                                        <label class="label">Mật khẩu</label>
-                                        <div class="control">
-                                            <input name="password"
-                                                   class="input <?php print !empty($errors['password']) ? 'is-danger' : ''; ?>"
-                                                   type="password" placeholder="Mật khẩu"
-                                                   value="<?php print !empty($_POST['password']) ? $_POST['password'] : ""; ?>"
-                                            >
-                                            <?php if (!empty($errors['password'])): ?>
-                                                <p class="help is-danger"><?php print $errors['password']; ?></p>
-                                            <?php endif; ?>
-                                        </div>
-                                    </div>
-=======
                                 <div class="card-header-title">Cập nhật sinh
                                     viên
                                 </div>
@@ -224,9 +117,9 @@ require_once "header.php";
                                 <?php
                                 if ( ! empty($message)): ?>
                                     <article
-                                            class="message <?php
-                                            print $message['type'] == 'error'
-                                              ? 'is-danger' : 'is-success' ?>">
+                                      class="message <?php
+                                      print $message['type'] == 'error'
+                                        ? 'is-danger' : 'is-success' ?>">
                                         <div class="message-body">
                                             <?php
                                             print $message['message']; ?>
@@ -238,20 +131,10 @@ require_once "header.php";
                                 print path('/index.php?p=admin/students/edit&id=')
                                   .$id; ?>">
 
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 
                                     <div class="field">
                                         <label class="label">Họ</label>
                                         <div class="control">
-<<<<<<< HEAD
-                                            <input value="<?php print $values['ho']; ?>"
-                                                   name="ho" class="input"
-                                                   type="text" placeholder="Họ"
-                                            >
-                                            <?php if (!empty($errors['ho'])): ?>
-                                                <p class="help is-danger"><?php print $errors['ho']; ?></p>
-                                            <?php endif; ?>
-=======
                                             <input value="<?php
                                             print $values['ho']; ?>"
                                                    name="ho" class="input"
@@ -263,21 +146,11 @@ require_once "header.php";
                                                     print $errors['ho']; ?></p>
                                             <?php
                                             endif; ?>
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
                                         </div>
                                     </div>
                                     <div class="field">
                                         <label class="label">Tên</label>
                                         <div class="control">
-<<<<<<< HEAD
-                                            <input value="<?php print $values['ten']; ?>"
-                                                   name="ten" class="input"
-                                                   type="text" placeholder="Tên"
-                                            >
-                                            <?php if (!empty($errors['ten'])): ?>
-                                                <p class="help is-danger"><?php print $errors['ten']; ?></p>
-                                            <?php endif; ?>
-=======
                                             <input value="<?php
                                             print $values['ten']; ?>"
                                                    name="ten" class="input"
@@ -289,7 +162,6 @@ require_once "header.php";
                                                     print $errors['ten']; ?></p>
                                             <?php
                                             endif; ?>
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 
                                         </div>
                                     </div>
@@ -297,29 +169,6 @@ require_once "header.php";
                                     <div class="field">
                                         <label class="label">Ngày sinh</label>
                                         <div class="control">
-<<<<<<< HEAD
-                                            <input value="<?php print !empty($_POST['ngay_sinh']) ? $_POST['ngay_sinh'] : ""; ?>"
-                                                   name="ngay_sinh" class="input"
-                                                   type="text" placeholder="DD/MM/YYYY"
-                                            >
-                                        </div>
-                                        <?php if (!empty($errors['ngay_sinh'])): ?>
-                                            <p class="help is-danger"><?php print $errors['ngay_sinh']; ?></p>
-                                        <?php endif; ?>
-                                    </div>
-
-                                    <div class="field">
-                                        <label class="label">Mã Sinh viên</label>
-                                        <div class="control">
-                                            <input value="<?php print $values['ma_sinh_vien']; ?>"
-                                                   name="ma_sinh_vien"
-                                                   class="input <?php print !empty($errors['ma_sinh_vien']) ? 'is-danger' : ''; ?>"
-                                                   type="text" placeholder="Mã sinh viên"
-                                            >
-                                            <?php if (!empty($errors['ma_sinh_vien'])): ?>
-                                                <p class="help is-danger"><?php print $errors['ma_sinh_vien']; ?></p>
-                                            <?php endif; ?>
-=======
                                             <input value="<?php
                                             print ! empty($_POST['ngay_sinh'])
                                               ? $_POST['ngay_sinh']
@@ -357,7 +206,6 @@ require_once "header.php";
                                                     print $errors['ma_sinh_vien']; ?></p>
                                             <?php
                                             endif; ?>
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
 
                                         </div>
                                     </div>
@@ -366,19 +214,6 @@ require_once "header.php";
                                         <label class="label">Lớp</label>
                                         <div class="control">
                                             <input name="lop"
-<<<<<<< HEAD
-                                                   class="input <?php print !empty($errors['lop']) ? 'is-danger' : ''; ?>"
-                                                   type="text" placeholder="Lớp"
-                                                   value="<?php print $values['lop']; ?>">
-                                        </div>
-                                        <?php if (!empty($errors['lop'])): ?>
-                                            <p class="help is-danger"><?php print $errors['lop']; ?></p>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="field">
-                                        <div class="control">
-                                            <button type="submit" class="button is-link">Cập nhật</button>
-=======
                                                    class="input <?php
                                                    print ! empty($errors['lop'])
                                                      ? 'is-danger' : ''; ?>"
@@ -399,7 +234,6 @@ require_once "header.php";
                                                     class="button is-link">Cập
                                                 nhật
                                             </button>
->>>>>>> 265236428fd06e84c05ae3114161e9c14886aea2
                                         </div>
                                     </div>
                                 </form>
